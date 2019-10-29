db9e80c2830cd5b16607b261498d4556b8b2962a
Pawel Kedziora <pakedziora@gmail.com>
2019-10-29 21:11:28 +0000

    Navbar with temp logo

--- components/VideoList.vue
+++ components/VideoList.vue
@@ -1,5 +1,12 @@
 <template>
 <div>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="/images/logo.png" width="125" height="31">
+    </a>
+  </div>
+</nav>
 <!-- <h1>Ungropued</h1>
   <ul>
     <li v-for="video in videos" :key="video.id">
@@ -8,7 +15,6 @@
       <a :href="video.url"><img :src="video.thumbnails.medium.url" /></a>
     </li>
   </ul> -->
-  <hr/>
   <h1>Gropued</h1>
   <ul>
     <li v-for="group in videosGrouped" :key="group.id">
@@ -23,6 +29,8 @@
           <h1><a :href="video.url">{{ video.title }}</a></h1>
           <!-- <a :href="video.url"><img :src="video.thumbnails.medium.url" /></a> -->
           <iframe loading="lazy" type="text/html" width="640" height="360"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
           :src="'https://www.youtube.com/embed/' + video.id" frameborder="0"
           :srcdoc="'<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/' + video.id + '?autoplay=1><img src=https://img.youtube.com/vi/' + video.id + '/hqdefault.jpg alt=\'' + video.description + '\'><span>▶</span></a>'"></iframe>
         </li>
