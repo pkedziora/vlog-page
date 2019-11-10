4fa3895de6fcdb264e156256070707e48f06545c
Pawel Kedziora <pakedziora@gmail.com>
2019-11-10 19:53:57 +0000

    Test no thumbnails

--- components/VideoItem.vue
+++ components/VideoItem.vue
@@ -6,8 +6,7 @@
       <iframe loading="lazy" type="text/html"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
-          :src="'https://www.youtube.com/embed/' + video.id" frameborder="0"
-          :srcdoc="'<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/' + video.id + '?autoplay=1><img src=https://img.youtube.com/vi/' + video.id + '/hqdefault.jpg alt=\'' + video.description + '\'><span>▶</span></a>'"></iframe>
+          :src="'https://www.youtube.com/embed/' + video.id" frameborder="0"></iframe>
 
   </div>
   <div class="card-content">
