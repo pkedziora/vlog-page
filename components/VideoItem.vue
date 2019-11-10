994a62b6251a9012b6e933d4d280515d4a57d2f5
Pawel Kedziora <pakedziora@gmail.com>
2019-11-10 20:02:51 +0000

    Cleanup

--- components/VideoItem.vue
+++ components/VideoItem.vue
@@ -30,16 +30,6 @@
     </div>
   </div>
 </div>
-
-<!-- <div class="videoItemContainer">
-          <p><a :href="video.url">{{ video.title }}</a> - {{ video.date | formatDate}}</p>
-          <iframe loading="lazy" type="text/html" width="640" height="360"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-          :src="'https://www.youtube.com/embed/' + video.id" frameborder="0"
-          :srcdoc="'<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/' + video.id + '?autoplay=1><img src=https://img.youtube.com/vi/' + video.id + '/hqdefault.jpg alt=\'' + video.description + '\'><span>▶</span></a>'"></iframe>
-
-</div> -->
 </template>
 
 <script>
